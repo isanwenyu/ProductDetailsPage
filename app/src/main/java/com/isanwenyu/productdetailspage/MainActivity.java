9f9e5a35e2e717b6bfda5ca525ee0b45aa17906c
isanwenyu <isanwenyu@163.com>
2016-09-12 18:28:38 +0800

    add app module

---
+++ app/src/main/java/com/isanwenyu/productdetailspage/MainActivity.java
@@ -0,0 +1,18 @@
+package com.isanwenyu.productdetailspage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 带title的基础布局处理
+ * Created by isanwenyu on 2016/9/12.
+ * Copyright (c) 2016 isanwenyu@163.com. All rights reserved.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
